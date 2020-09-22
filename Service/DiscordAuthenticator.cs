1d4f0eb406122938f22d9b1b0f4366eaa2ce1112
adamtuechler <adamtuechler@gmail.com>
2020-09-22 13:00:45 +1000

    Removed config files for production and dev

--- Service/DiscordAuthenticator.cs
+++ Service/DiscordAuthenticator.cs
@@ -4,6 +4,7 @@ using System.Net.Http;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Text.Json;
+using Microsoft.Extensions.Configuration;
 
 using csharp_api.Database;
 using csharp_api.Model.User;
@@ -14,22 +15,27 @@ namespace csharp_api.Services.Discord
 {
     public class DiscordAuthenticator : IAuthenticationService<Profile>
     {
-        private readonly string client_id;
-        private readonly string client_secret;
+        private readonly string _client_id;
+        private readonly string _client_secret;
         private readonly HttpClient client = new HttpClient();
-        private readonly string discordTokenURI;
-        private readonly string discordUserURI;
+        private readonly string _discordTokenURI;
+        private readonly string _discordUserURI;
         private readonly IDatabase _database;
+        private readonly IConfiguration _config;
+        private readonly string _redirect_uri;
 
-        public DiscordAuthenticator(IDatabase database)
+        public DiscordAuthenticator(IDatabase database, IConfiguration config)
         {
             _database = database;
+            _config = config.GetSection("DiscordSecrets");
 
-            // Todo Configuration or injection (or both)
-            this.client_id = "754956360424226882";
-            this.client_secret = "n_dmzWF57NzbjPro5tN1DmqnnjeBg6NB";
-            this.discordTokenURI = "https://discord.com/api/oauth2/token";
-            this.discordUserURI = "https://discord.com/api/users/@me";
+            // Read client_id and secret from configuration
+            this._client_id = _config["client_id"]; 
+            this._client_secret = _config["client_secret"];
+            this._redirect_uri = _config["redirect_uri"];
+
+            this._discordTokenURI = "https://discord.com/api/oauth2/token";
+            this._discordUserURI = "https://discord.com/api/users/@me";
         }
 
         public async Task<Profile> Authenticate(string code)
@@ -72,18 +78,18 @@ namespace csharp_api.Services.Discord
         {
             // Request body collection
             Dictionary<string, string> requestBody = new Dictionary<string, string>();
-            requestBody.Add("client_id", this.client_id);
-            requestBody.Add("client_secret", this.client_secret);
+            requestBody.Add("client_id", this._client_id);
+            requestBody.Add("client_secret", this._client_secret);
             requestBody.Add("grant_type", "authorization_code");
             requestBody.Add("code", code);
-            requestBody.Add("redirect_uri", "http://localhost:4200/auth?ref=discord");
+            requestBody.Add("redirect_uri", _redirect_uri);
             requestBody.Add("scope", "identify");
 
             // Encode the body content
             var bodyContent = new FormUrlEncodedContent(requestBody);
 
             // Build the request
-            var tokenRequest = new HttpRequestMessage(HttpMethod.Post, this.discordTokenURI)
+            var tokenRequest = new HttpRequestMessage(HttpMethod.Post, this._discordTokenURI)
             {
                 Content = bodyContent
             };
@@ -101,7 +107,7 @@ namespace csharp_api.Services.Discord
         private async Task<DiscordUser> _UserRequest(DiscordToken discordToken)
         {
             // Request a user profile using access token
-            HttpRequestMessage userRequest = new HttpRequestMessage(HttpMethod.Get, discordUserURI);
+            HttpRequestMessage userRequest = new HttpRequestMessage(HttpMethod.Get, _discordUserURI);
             userRequest.Headers.Authorization = new AuthenticationHeaderValue("Bearer", discordToken.access_token);
 
             var httpUserResponse = await this.client.SendAsync(userRequest);
